fd805db5d4d81031eeae3d57fd5b09d9afca2d28
Олег Мисяк <oleg.misyak@gmail.com>
2016-01-21 21:38:28 +0200

    Initial Commit

---
+++ FirstTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTest
+//
+//  Created by Oleg Misyak on 21/01/16.
+//  Copyright © 2016 Oleg Misyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
